e2e057abc406dd400f07f12a0b5191ebf069fd6d
Yang YANG <pdshuimu@gmail.com>
2018-07-03 16:41:08 +0800

    add data description

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' The Real GDP Growth Rate of Different Countries, from 1993Q1 to 2008Q1.
+#'
+#' A dataset containing the Real GDP Growth Rate of 25 countries or regions.
+#'
+#' @format A data frame with 61 rows and 26 variables:
+#' \describe{
+#'   \item{date}{Year-Quarter Date Variable}
+#'   ...
+#' }
+#' @source Cheng Hsiao, H. Steve Ching, and Shui Ki Wan (2012), "A panel data approach for program evaluation: measuring the benefits of political and economic integration of Hong kong with mainland China", Journal of Applied Econometrics.
+#' @export
+"hcw_data"
